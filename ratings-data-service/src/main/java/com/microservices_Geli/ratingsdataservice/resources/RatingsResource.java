28bc3ef5b2c1ad3f5b97e331505435de60b3194c
Geli Yang <gyang5@villanova.edu>
2022-10-09 10:46:55 -0400

    Implement Rating class, setup a get controller in RatingsResource

---
+++ ratings-data-service/src/main/java/com/microservices_Geli/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,17 @@
+package com.microservices_Geli.ratingsdataservice.resources;
+
+import com.microservices_Geli.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+}
