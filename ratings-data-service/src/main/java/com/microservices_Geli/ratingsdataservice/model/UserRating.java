01e51e332250cccd3cfb62a505ee7e1e307048ec
Geli Yang <gyang5@villanova.edu>
2022-10-10 13:50:43 -0400

    Implement getUserRating method in controller.

---
+++ ratings-data-service/src/main/java/com/microservices_Geli/ratingsdataservice/model/UserRating.java
@@ -0,0 +1,9 @@
+package com.microservices_Geli.ratingsdataservice.model;
+
+import java.util.List;
+
+public class UserRating {
+    private String userId;
+    private List<Rating> ratings;
+
+}
--- ratings-data-service/src/main/java/com/microservices_Geli/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/microservices_Geli/ratingsdataservice/resources/RatingsResource.java
@@ -1,6 +1,7 @@
 package com.microservices_Geli.ratingsdataservice.resources;
 
 import com.microservices_Geli.ratingsdataservice.model.Rating;
+import com.microservices_Geli.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +15,12 @@ public class RatingsResource {
         return new Rating(movieId, 4);
     }
 
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
 }
