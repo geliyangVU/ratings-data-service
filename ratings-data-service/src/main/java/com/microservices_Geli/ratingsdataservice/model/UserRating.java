067e6c37fabc22cec472ee5787b1d96bd238e3ea
Geli Yang <gyang5@villanova.edu>
2022-10-13 09:31:46 -0400

    Implement initData method in UserRating class to output dummy list
    Implement initData method in UserRating class to output dummy list

--- ratings-data-service/src/main/java/com/microservices_Geli/ratingsdataservice/model/UserRating.java
+++ ratings-data-service/src/main/java/com/microservices_Geli/ratingsdataservice/model/UserRating.java
@@ -1,5 +1,6 @@
 package com.microservices_Geli.ratingsdataservice.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class UserRating {
@@ -28,5 +29,14 @@ public class UserRating {
 
     public void setRatings(List<Rating> ratings) {
         this.ratings = ratings;
+    }
+
+
+    public void initData(String userId) {
+        this.setUserId(userId);
+        this.setRatings(Arrays.asList(
+                new Rating("100", 3),
+                new Rating("200", 4)
+        ));
     }
 }
